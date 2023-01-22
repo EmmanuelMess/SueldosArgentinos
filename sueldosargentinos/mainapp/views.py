76f08329ea2e1ef26bf5078d18a5b334a09ae2b8
EmmanuelMess <emmanuelbendavid@gmail.com>
2023-01-22 16:07:15 -0300

    Add search

--- sueldosargentinos/mainapp/views.py
+++ sueldosargentinos/mainapp/views.py
@@ -1,7 +1,24 @@
 from django.http import HttpResponse
 from django.template import Context, Engine, Template
 
+import django_filters
+
 from mainapp.models import Wage
+
+
+class ProductFilter(django_filters.FilterSet):
+    position = django_filters.CharFilter(lookup_expr='icontains')
+
+    class Meta:
+        model = Wage
+        fields = ['monthly']
+
+    @property
+    def qs(self):
+        parent = super().qs
+        parent = parent[:5]
+
+        return parent
 
 
 def index(request):
@@ -12,12 +29,21 @@ def index(request):
     aa: int = 758244
     position: str = "EXPERTO EN INGENIERÍIA SATELITAL Y PLATAFORMA - NIVEL B"
 
-    temp = Wage.objects.create(position=position, monthly=aa)
+    if not Wage.objects.all():
+        _ = Wage.objects.create(position=position, monthly=aa)
+        _ = Wage.objects.create(position=position, monthly=aa)
+        _ = Wage.objects.create(position=position, monthly=aa)
+        _ = Wage.objects.create(position=position, monthly=aa)
+
+    filter = ProductFilter(request.GET, queryset=Wage.objects.all())
+    show_dropdown = len(filter.qs) != 0
 
     context: Context = Context({
         "title": "Salarios Argentinos",
         "content": index_content_template.render(Context()),
-        "wage_list": [temp, temp, temp, temp]
+        "wage_list": Wage.objects.all()[:5],
+        "show_dropdown": show_dropdown,
+        "filter": filter,
     })
 
     return HttpResponse(index_content_template.render(context))
